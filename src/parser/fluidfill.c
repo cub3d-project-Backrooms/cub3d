27c293a1653ed583c4e37da213f5417728d32266
stripy1026 <inwoo1026@naver.com>
2022-04-22 18:55:57 +0900

    feat: :art: fluidfill done ! 4 map checked.

--- src/parser/fluidfill.c
+++ src/parser/fluidfill.c
@@ -16,15 +16,14 @@ t_ivec fluidfill__start_pos(t_string_arr this)
 		while (this[it.y][++it.x])
 		{
 			tile = this[it.y][it.x];
-			if (tile != MAPFMT__WALL && tile != MAPFMT__EMPTY)
+			if (tile != MAPFMT__WALL && tile != MAPFMT__EMPTY && tile != MAPFMT__FILL)
 				return (it);
 		}
 	}
-	std__panic("Map has no floor !");
-	return (it);
+	return (t_ivec){-100, -100};
 }
 
-void fluidfill__recursive(t_string_arr this, t_ivec node, char replace)
+void fluidfill__recursive(t_string_arr this, t_ivec node)
 {
 	t_mapformat tile;
 
@@ -34,42 +33,35 @@ void fluidfill__recursive(t_string_arr this, t_ivec node, char replace)
 	tile = this[node.y][node.x];
 	if (tile == MAPFMT__EMPTY || tile == MAPFMT__ERR)
 		std__panic("Invalid map");
-	if (this[node.y][node.x] == replace)
+	if (tile == MAPFMT__FILL)
 		return ;
 	if (tile == MAPFMT__WALL)
 		return ;
-	this[node.y][node.x] = replace;
-	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x, node.y + 1}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x, node.y - 1}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y + 1}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y - 1}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y + 1}, replace);
-	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y - 1}, replace);
+	this[node.y][node.x] = MAPFMT__FILL;
+	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y});
+	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y});
+	fluidfill__recursive(this, (t_ivec){node.x, node.y + 1});
+	fluidfill__recursive(this, (t_ivec){node.x, node.y - 1});
+	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y + 1});
+	fluidfill__recursive(this, (t_ivec){node.x + 1, node.y - 1});
+	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y + 1});
+	fluidfill__recursive(this, (t_ivec){node.x - 1, node.y - 1});
 }
 
 bool raw_map_arr__fluidfill(t_string_arr this, t_sizevec map_size)
 {
-	t_ivec it;
 	t_ivec start;
 	t_string_arr tmp;
 
 	printf("%sfluidfill algorithm is checking map validation ...%s\n", CYN, END);
 	tmp = str__new__arr(map_size.height, this);
-	it.y = -1;
-	while (tmp[++it.y])
-		printf("%s\n", tmp[it.y]);
-	printf("copyed\n");
-	start = fluidfill__start_pos(tmp);
-	fluidfill__recursive(tmp, start, '2');
-	it.y = -1;
-	while (tmp[++it.y])
-		printf("%s\n", tmp[it.y]);
-	printf("algo done\n");
-	it.y = -1;
-	while (this[++it.y])
-		printf("%s\n", this[it.y]);
+	while (true)
+	{
+		start = fluidfill__start_pos(tmp);
+		if (start.x < 0)
+			break ;
+		fluidfill__recursive(tmp, start);
+	}
 	str__delete__arr(&tmp);
 	return (true);
 }
