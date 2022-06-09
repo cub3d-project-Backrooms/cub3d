60677558f0c128754ec045e0ba35ffd08f1a174d
scarf <greenscarf005@gmail.com>
2022-06-09 16:34:15 +0900

    refactor: 파일로 나누기

--- config/packages.mk
+++ config/packages.mk
@@ -13,7 +13,8 @@
 # @Packages
 PKGS = renderer engine parser
 
-rendererV = raycast_tool raycast_floor camera image init minimap draw\
+rendererV = raycast_tool raycast_floor raycast_texture \
+			camera image init minimap draw\
 			raycast load_tex raycast_wall deinit dda__dist dda__step
 engineV = init keyinput movement run collision door
 parserV = map check__parser check__cubid parser check__cubfile cubid\
--- include/renderer.h
+++ include/renderer.h
@@ -93,6 +93,11 @@ void		floordata__raycast__set_dtexture_vector(t_floordata *this);
 void		renderer__draw__floor(t_renderer *this, t_floordata *vecs,
 				int current_x, int current_y);
 /*
+** < raycast_texture.c > */
+
+t_texdata	walldata__get_texdata(t_walldata *data, t_mapfmt tile_type);
+t_rgb		renderer__draw__wall_texture(t_renderer *this, t_walldata *data);
+/*
 ** < raycast_tool.c > */
 
 t_rgb		shade_color(int color, double divide);
@@ -107,6 +112,4 @@ void		walldata__raycast__set_dda_vector(t_walldata *this,
 				t_camera *camera, int current_x, t_world *world);
 void		walldata__draw__set_wall_data(t_walldata *this, t_camera *camera);
 void		walldata__draw__set_texture_data(t_walldata *this);
-t_texdata	walldata__get_texdata(t_walldata *data, t_mapfmt tile_type);
-t_rgb		renderer__draw__wall_texture(t_renderer *this, t_walldata *data);
 #endif  // __RENDERER_H__
