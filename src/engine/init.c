a78f008df8df4d7ec87a0baa1e4384f920c91d53
tkim <wpdldkdl77@naver.com>
2022-06-10 13:18:41 +0900

    #58 cub3d 시작시 마우스포인터 스크린 중앙으로 이동

--- src/engine/init.c
+++ src/engine/init.c
@@ -22,20 +22,17 @@ int mouse_hook(int x, int y, t_engine *this)
 	(void)x;
 	this->inputhandler.mouse_motion_size++;
 	mlx_mouse_get_pos(this->renderer.window, &this->inputhandler.x, &y );
-	//mlx_mouse_move(this->renderer.window, WIDTH / 2, HEIGHT / 2 );//스크린 정 중앙에 마우스 위치
-	//printf("%d %d\n", this->inputhandler.x, this->inputhandler.mouse_motion_size);
 	return (0);
 }
 
 static void	engine__init__input(t_engine *this)
 {
-	mlx_mouse_hide();
+	mlx_mouse_move(this->renderer.window, WIDTH / 2, HEIGHT / 2 );
 	mlx_hook(this->renderer.window, X11EVENTS__KeyPress, X11MASKS__KeyPressMask,
 		&inputhandler__key_press, &this->inputhandler);
 	mlx_hook(this->renderer.window, X11EVENTS__KeyRelease,
 		X11MASKS__KeyReleaseMask, &inputhandler__key_release,
 		&this->inputhandler);
-	//mlx_mouse_hook(this->renderer.window, mouse_hook,this);
 	mlx_hook(this->renderer.window, X11EVENTS__MotionNotify, X11MASKS__ButtonMotionMask, &mouse_hook, this);
 	mlx_hook(this->renderer.window, X11EVENTS__DestroyNotify,
 		X11MASKS__NoEventMask, &engine__deinit, this);
